f5e090187026424dfaaf2042cb0704a801b5d84c
xzwb <1478863479@qq.com>
2019-10-30 19:56:21 +0800

    静态代码块和静态方法

--- Demo.java
+++ Demo.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("开始!!");
         Demo demo = new Demo();
         demo.printf();
         Demo demo1 = new Demo("刘备");
         demo1.printf();
         Demo demo2 = new Demo("刘备", 25);
         demo2.printf();
+        int a = Demo.add(1, 2);
+        System.out.println("a = " + a);
+
     }
 }
 
@@ -15,6 +19,16 @@ public class Demo {
         System.out.println("hello world!!");
     }    
     
+    // 静态方法
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // 静态代码块在一个类第一次被加载时执行
+    static {
+        System.out.println("静态代码块");
+    }
+
     // 构造代码块
     {
         System.out.println("start!!");    
