b07514bdfd56cb21a17ffa3ea058a80e0f167b2a
xzwb <1478863479@qq.com>
2019-10-31 20:34:25 +0800

    多态

--- more.java
+++ more.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Animals animal = new Cat();
         animal.eat();
+        Cat cat = new Cat();
+        eat(cat);
+    }
+    
+    // 父类可以接受子类对象
+    public static void eat(Animals animal) {
+        animal.eat();
     }
 }
 
