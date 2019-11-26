c8aa29190767532fc4100d23f5e660aedb6880d2
xzwb <1478863479@qq.com>
2019-11-26 18:46:42 +0800

    数学

---
+++ src/MathTest.java
@@ -0,0 +1,9 @@
+public class MathTest {
+    public static void main(String[] args) {
+        // 0.0到1.0之间的随机数
+        System.out.println(Math.random());
+        // 求e的多少次方
+        System.out.println(Math.exp(2));
+
+    }
+}
