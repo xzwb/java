8abf29d753a3c10d2d3324acb055e30f3a85095a
xzwb <1478863479@qq.com>
2019-11-27 15:39:25 +0800

    迭代器

--- src/IteratorTest.java
+++ src/IteratorTest.java
@@ -8,15 +8,18 @@ public class IteratorTest {
         collection.add("15");
         collection.add("25");
         Iterator iterator = collection.iterator();
+        iterator.forEachRemaining(obj->System.out.println(obj));
         while (iterator.hasNext()) {
+            System.out.println("执行了");
             String str = (String)iterator.next();
             System.out.println(str);
             if (str.equals("25")) {
-                /*iterator.remove();*/
-                /*collection.remove(str);*/
+                iterator.remove();
+                collection.remove(str);
                 collection.remove("10");
             }
         }
         System.out.println(collection);
+        iterator.forEachRemaining(obj->System.out.println(obj));
     }
 }
